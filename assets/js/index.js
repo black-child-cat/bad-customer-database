5ed933661eea3ed3121069ee490a45bda40c3b4d
氏家大介 <80656568+ujike-daisuke@users.noreply.github.com>
2023-06-23 16:58:43 +0900

    仕様変更

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,11 @@
-$(function () {});
+function postData() {
+    var formdata = $('#myForm').serialize();
+    $.ajax({
+        type: "POST",
+        url: "https://script.google.com/macros/s/AKfycbxPE50blPbkr5XUs0kdckHfqYH7bvPJFsblNlaydRxgxLmqChQNJI1OkXwL4vUevLw/exec",
+        data: formdata,
+        success: function(data){
+            alert('データ送信に成功しました。');
+        }
+    });
+}
