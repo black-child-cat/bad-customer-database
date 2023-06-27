f00e5fe440a1e5509a654747bf75e5ec2b8c3ce8
UD <46685886+black-child-cat@users.noreply.github.com>
2023-06-27 23:44:09 +0900

    修正

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,25 @@
-function postData() {
-    var formdata = $('#myForm').serialize();
-    $.ajax({
-        type: "POST",
-        url: "https://script.google.com/macros/s/AKfycbxPE50blPbkr5XUs0kdckHfqYH7bvPJFsblNlaydRxgxLmqChQNJI1OkXwL4vUevLw/exec",
-        data: formdata,
-        success: function(data){
-            alert('データ送信に成功しました。');
-        }
-    });
-}
+$(document).ready(function () {
+	$("#myForm").on("submit", function (e) {
+		e.preventDefault();
+
+		$.ajax({
+			type: "POST",
+			url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSd17JP12Gt74VwS16cBsbFV5bvjKOt-N0BgTzBiEmBXXEDcnA/formResponse",
+			data: $(this).serialize(),
+			success: function (response) {
+				// レスポンスに基づいて何かを行う
+				console.log(response);
+				alert("データ送信に成功しました。");
+				$("#myForm")[0].reset();
+				$(".toast").toast("show");
+			},
+			error: function (error) {
+				// エラー時の処理
+				console.log(error);
+				alert(JSON.stringify(error));
+				$("#myForm")[0].reset();
+				$(".toast").toast("show");
+			},
+		});
+	});
+});
